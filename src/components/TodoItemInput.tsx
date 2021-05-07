4e8871e8fdf755b06d9f7e7922104f9d9856e4ed
Christina Kashevar <christi_k@mail.ru>
2021-05-07 23:02:34 +0300

    refactor: delete import

--- src/components/TodoItemInput.tsx
+++ src/components/TodoItemInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, {  useRef } from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import classes from './TodoItemInput.module.css';
 
